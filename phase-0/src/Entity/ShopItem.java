8b709892587275aaa1a75bbd029b15f5b6ae2102
Fatimeh Hassan <syeda.fatimeh@gmail.com>
2021-10-09 16:48:37 -0400

    simple/rough implementation of the ShopItem and Shop entity classes

---
+++ phase-0/src/Entity/ShopItem.java
@@ -0,0 +1,19 @@
+package Entity;
+
+public class ShopItem {
+    private String name;
+    private int cost;
+
+    public ShopItem(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCost() {
+        return this.cost;
+    }
+}
