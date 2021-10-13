4983ba44aaff712b0247f9b61b88337cc573f16d
pjuntae <jpark0521@gmail.com>
2021-10-13 13:00:56 -0400

    Implemented Clickable

---
+++ phase-0/src/main/java/usecases/Clickable.java
@@ -0,0 +1,15 @@
+package main.java.usecases;
+
+/**
+ * This interface is to be implemented on all clickable entities
+ * @author Juntae Park
+ * @since 12 October 2021
+*/
+
+public interface Clickable {
+    public boolean clicked(int mouseX, int mouseY);
+
+    public int[][] getLocation();
+
+    public void act();
+}
